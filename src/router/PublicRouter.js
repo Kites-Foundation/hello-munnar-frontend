f0098dbfa4fe1fb9f3274ad917db3374822bf4c2
AJIL-PAUL <ajilpaul.268@gmail.com>
2021-01-30 19:09:56 +0530

    added experience view

--- src/router/PublicRouter.js
+++ src/router/PublicRouter.js
@@ -2,9 +2,11 @@ import { useRoutes } from "hookrouter";
 import React from "react";
 import Home from "../components/Home/index";
 import NavBar from "../components/Common/NavBar";
+import Experiences from "../components/Experiences/Experiences";
 
 const routes = {
     "/": () => <Home />,
+    "/experiences": () => <Experiences />,
 };
 
 export default function PublicRouter() {
