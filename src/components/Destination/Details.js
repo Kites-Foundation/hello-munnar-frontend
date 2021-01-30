61d86b34ba53260412a4e00fc84c60c0d6977d3e
Sivanandini B Nair <sivanandinias239@gmail.com>
2021-01-30 20:31:40 +0530

    style changes

--- src/components/Destination/Details.js
+++ src/components/Destination/Details.js
@@ -8,7 +8,7 @@ export default function Details({id}) {
         <button className="bg-red-400 border-red-500 border-2 hover:bg-red-300 text-grey px-4 rounded-none">
         #tag2
         </button>
-        <div className = "text-grey justify-items-center my-10 box-border md:box-content">
+        <div className = "text-grey justify-items-center my-5 box-border md:box-content">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
         </div>
     </div>
