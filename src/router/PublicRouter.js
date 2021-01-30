9a2c2f2dd1c4e326ee32327aee7de2726baa1c4d
Fayazazeemp <fayazazeem444555@gmail.com>
2021-01-31 04:54:14 +0530

    error corrected

--- src/router/PublicRouter.js
+++ src/router/PublicRouter.js
@@ -6,7 +6,6 @@ import NavBar from "../components/Common/NavBar";
 import Experiences from "../components/Experiences/Experiences";
 import NearMe from "../components/NearMe/index.js";
 
-
 const routes = {
     "/": () => <Home />,
     "/experiences": () => <Experiences />,
