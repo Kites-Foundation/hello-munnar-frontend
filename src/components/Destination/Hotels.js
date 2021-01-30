e16d065a2dc2835ac978e21f4e0ff3d215569cfa
aashnanaushad <aashnanaushad@gmail.com>
2021-01-30 18:36:24 +0530

    Added UI design for activities tab in destination module

--- src/components/Destination/Hotels.js
+++ src/components/Destination/Hotels.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "../../assets/images/dummy.jpg"
+import Image from "../../assets/images/dummy.jpg";
 import Star from "../Common/Star";
 
 export default function Hotels() {
@@ -7,15 +7,20 @@ export default function Hotels() {
         <div className="center max-w-6xl mx-auto">
             <div className="mt-12 grid gap-5 max-w-lg mx-auto lg:grid-cols-4 md:grid-cols-3 md:max-w-none sm:mx-8">
                 <div className="overflow-hidden rounded-lg shadow-lg">
-
                     <a href="#">
-                        <img alt="Placeholder" className="block h-auto w-full" src={Image} />
+                        <img
+                            alt="Placeholder"
+                            className="block h-auto w-full"
+                            src={Image}
+                        />
                     </a>
                     <div className="flex items-center justify-between leading-tight p-2 md:p-4 mt-55">
                         <h1 className="text-lg">
-                            <a className="no-underline hover:underline text-black" href="#">
+                            <a
+                                className="no-underline hover:underline text-black"
+                                href="#">
                                 Name
-                        </a>
+                            </a>
                         </h1>
                         <svg
                             aria-hidden="true"
@@ -36,16 +41,13 @@ export default function Hotels() {
                     <div className="flex justify-end mt-5">
                         <p className="text-grey-darker text-sm">
                             <span className="flex">
-                                <Star
-                                    num={3}
-                                    dim={4}
-                                />
+                                <Star num={3} dim={4} />
                             </span>
                         </p>
                     </div>
                     <div className="flex flex-row leading-none p-2 md:p-4">
-
-                        <svg aria-hidden="true"
+                        <svg
+                            aria-hidden="true"
                             focusable="false"
                             data-prefix="fas"
                             data-icon="car-side"
@@ -55,16 +57,22 @@ export default function Hotels() {
                             role="img"
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 640 512">
-                            <path fill="currentColor" d="M544 192h-16L419.22 56.02A64.025 64.025 0 0 0 369.24 32H155.33c-26.17 0-49.7 15.93-59.42 40.23L48 194.26C20.44 201.4 0 226.21 0 256v112c0 8.84 7.16 16 16 16h48c0 53.02 42.98 96 96 96s96-42.98 96-96h128c0 53.02 42.98 96 96 96s96-42.98 96-96h48c8.84 0 16-7.16 16-16v-80c0-53.02-42.98-96-96-96zM160 432c-26.47 0-48-21.53-48-48s21.53-48 48-48 48 21.53 48 48-21.53 48-48 48zm72-240H116.93l38.4-96H232v96zm48 0V96h89.24l76.8 96H280zm200 240c-26.47 0-48-21.53-48-48s21.53-48 48-48 48 21.53 48 48-21.53 48-48 48z"></path></svg>
+                            <path
+                                fill="currentColor"
+                                d="M544 192h-16L419.22 56.02A64.025 64.025 0 0 0 369.24 32H155.33c-26.17 0-49.7 15.93-59.42 40.23L48 194.26C20.44 201.4 0 226.21 0 256v112c0 8.84 7.16 16 16 16h48c0 53.02 42.98 96 96 96s96-42.98 96-96h128c0 53.02 42.98 96 96 96s96-42.98 96-96h48c8.84 0 16-7.16 16-16v-80c0-53.02-42.98-96-96-96zM160 432c-26.47 0-48-21.53-48-48s21.53-48 48-48 48 21.53 48 48-21.53 48-48 48zm72-240H116.93l38.4-96H232v96zm48 0V96h89.24l76.8 96H280zm200 240c-26.47 0-48-21.53-48-48s21.53-48 48-48 48 21.53 48 48-21.53 48-48 48z"></path>
+                        </svg>
 
-                        <p className="ml-2 text-sm">
-                            10 min 2 km
-                        </p>
+                        <p className="ml-2 text-sm">10 min 2 km</p>
                     </div>
                     <div>
-                        <p className="ml-2 text-sm">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</p>
+                        <p className="ml-2 text-sm">
+                            Neque porro quisquam est qui dolorem ipsum quia
+                            dolor sit amet
+                        </p>
                     </div>
-                    <button className="block mx-auto my-1 uppercase px-4 py-2 rounded-full bg-green-600 text-blue-50 max-w-max shadow-sm hover:shadow-lg">Check Availability</button>
+                    <button className="block mx-auto my-1 uppercase px-4 py-2 rounded-full bg-green-600 text-blue-50 max-w-max shadow-sm hover:shadow-lg">
+                        Check Availability
+                    </button>
                 </div>
             </div>
         </div>
