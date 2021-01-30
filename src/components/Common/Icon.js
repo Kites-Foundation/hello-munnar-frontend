907e2c544b2e8ec6cd24dff95aa0ae06222f1f91
Savio Martin <saviomartin2007@gmail.com>
2021-01-30 19:55:54 +0530

    User profile components

--- src/components/Common/Icon.js
+++ src/components/Common/Icon.js
@@ -5,12 +5,23 @@ import { ReactComponent as Earth } from "../../assets/images/earth.svg";
 import { ReactComponent as Diary } from "../../assets/images/diary.svg";
 import { ReactComponent as User } from "../../assets/images/user.svg";
 
+import { ReactComponent as AddUser } from "../../assets/images/add-user.svg";
+import { ReactComponent as EditUser } from "../../assets/images/edit-user.svg";
+import { ReactComponent as Lock } from "../../assets/images/lock.svg";
+import { ReactComponent as Star } from "../../assets/images/star.svg";
+import { ReactComponent as UserFilled } from "../../assets/images/user-filled.svg";
+
 const icons = {
     nearMe: NearMe,
     ticket: Ticket,
     earth: Earth,
     diary: Diary,
     user: User,
+    addUser: AddUser,
+    editUser: EditUser,
+    lock: Lock,
+    star: Star,
+    userFilled: UserFilled,
 };
 
 export default function Icon({ name, size = 10, color = "gray-800" }) {
