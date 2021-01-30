cd67d7154c41fee61d1e9c02953136cc0ddad086
Nikhil Nath R <nikhilnath1000@gmail.com>
2021-01-30 21:53:17 +0530

    Add static images
    - Add overlay to image

--- src/components/Common/BackButton.js
+++ src/components/Common/BackButton.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Icon from "./Icon";
-export default function BackButton() {
+export default function BackButton({ color = "black" }) {
     return (
         <div className="absolute z-10 top-3 left-3 ">
-            <Icon name="back" color="black" size={8} />
+            <Icon name="back" color={color} size={8} />
         </div>
     );
 }
--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-import dummy from "../../assets/images/dummy.jpg";
+import dummy from "../../assets/images/trek-bg.jpg";
+import trek1 from "../../assets/images/trek-1.jpg";
+import trek2 from "../../assets/images/trek-2.jpg";
 import Icon from "../Common/Icon";
 import LocationCard from "./LocationCard";
 import BackButton from "../Common/BackButton";
@@ -7,15 +9,18 @@ import BackButton from "../Common/BackButton";
 export default function Experience({ id }) {
     return (
         <div className="">
-            <BackButton />
+            <BackButton color="white" />
             <div className="relative">
+                <div className="absolute w-full h-full bg-black bg-opacity-50" />
                 <img
                     src={dummy}
                     alt=""
                     className="h-56 w-full object-cover mx-auto"
                 />
                 <div className="absolute bottom-0 left-0 p-3 w-full flex justify-between">
-                    <h2 className=" text-3xl font-semibold">Trekking</h2>
+                    <h2 className=" text-3xl font-semibold text-white">
+                        Trekking
+                    </h2>
                     <div className="flex items-center box-border bg-indigo-600 bg-opacity-25 justify-center w-10 h-10 rounded-full border-indigo-600 border-2">
                         <Icon name="location" color="indigo-600" size={7} />
                     </div>
@@ -25,8 +30,8 @@ export default function Experience({ id }) {
                 <Icon name="filter" size={9} />
             </div>
             <div className="px-8">
-                <LocationCard />
-                <LocationCard />
+                <LocationCard img={trek1} />
+                <LocationCard img={trek2} />
             </div>
         </div>
     );
