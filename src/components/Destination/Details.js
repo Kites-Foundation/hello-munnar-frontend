3485aad3fe345cd085f61323ec78e51ed3ccc8f3
Sivanandini B Nair <sivanandinias239@gmail.com>
2021-01-30 13:25:38 +0530

    added destination detail page

--- src/components/Destination/Details.js
+++ src/components/Destination/Details.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-export default function Details() {
-    return <div>Details</div>;
+export default function Details({id}) {
+    return <div className = "mx-5 my-5">
+        <button className="justify-content: flex-start bg-yellow-500 border-red-500 border-2 hover:bg-yellow-400 text-grey font-bold py-2 px-4 rounded-none ">
+        #tag1
+        </button> &nbsp;&nbsp;
+        <button className="bg-red-400 border-red-500 border-2 hover:bg-red-300 text-grey font-bold py-2 px-4 rounded-none">
+        #tag1
+        </button>
+        <div className = "text-grey justify-items-center my-10 box-border md:box-content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+        </div>
+    </div>
+    
 }
