f05fbde826fa91c8ed7e9250adc9dd6958129032
Savio Martin <61895712+saviomartin@users.noreply.github.com>
2021-01-30 11:13:26 +0530

    Create index.js

---
+++ src/components/User/UserHome/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserHome = () => {
+    return (
+        <div className="user" >
+            <h1>UserHome</h1>
+        </div>
+    )
+}
+
+export default UserHome
