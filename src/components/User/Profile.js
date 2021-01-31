dd8bc491ba37608ba61da9b4b47ea268cf3544d9
Savio Martin <saviomartin2007@gmail.com>
2021-01-31 11:06:38 +0530

    Profile pages are updated

--- src/components/User/Profile.js
+++ src/components/User/Profile.js
@@ -1,8 +1,6 @@
 import React from "react";
-import Input from "./Common/Input";
 import Icon from "../Common/Icon";
-import PhoneNumberInput from "./Common/PhoneNumberInput";
-// import placeholder from "../../assets/images/dummy.jpg";
+import ProgressBar from "./Common/Progressbar";
 
 const UserHome = () => {
     const name = "John Doe";
@@ -88,6 +86,53 @@ const UserHome = () => {
                                 backgroundPosition: "center",
                             }}></div>
                     </div>
+                </div>
+            </div>
+
+            <div className="mt-4 flex flex-center w-full h-30 justify-center">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    height="60px"
+                    width="60px"
+                    stroke="#52BA81">
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
+                    />
+                </svg>
+                <div className="p-2 w-8/12">
+                    <ProgressBar progress="20%" color="green" />
+                    <h3>20 of 256 badges completed</h3>
+                </div>
+            </div>
+            <div className="flex flex-center w-full h-30 justify-center">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    height="60px"
+                    width="60px"
+                    stroke="#F49E2D">
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                    />
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                </svg>
+                <div className="p-2 w-8/12">
+                    <ProgressBar progress="10%" color="yellow" />
+                    <h3>30 of 360 locations visited</h3>
                 </div>
             </div>
         </div>
