8c7ffdcfa761e284f90bb953e101ee4a5bd1b637
bodhisha <bodhisha@gmail.com>
2021-01-31 11:50:07 +0530

    conflict resolve

--- src/components/Destination/index.js
+++ src/components/Destination/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import DestinationNav from "./DestinationNav";
 import Icon from "../Common/Icon";
-import data from '../Explore/assets/data/data.json';
+import data from "../Explore/assets/data/data.json";
 
 export default function Destination(id) {
     let dest = [];
-    data.forEach(element => {
-        element.destinations.forEach(item => {
+    data.forEach((element) => {
+        element.destinations.forEach((item) => {
             if (item.id === id.id) {
                 dest.push(item);
             }
-        })
+        });
     });
     return (
         <div className="relative z-20 min-h-screen ">
@@ -20,9 +20,7 @@ export default function Destination(id) {
                 className="h-64 sm:bg-cover mx-auto lg:rounded-lg "
             />
             <div className="flex justify-between">
-                <div className="text-2xl font-semibold m-4">
-                    {dest[0].name}
-                </div>
+                <div className="text-2xl font-semibold m-4">{dest[0].name}</div>
                 <div className="my-auto mr-2">
                     <Icon name="plus" color={"black"} size={6} />
                 </div>
