2cef19ce4dd8483149545c3b7bb709dc59211257
Fayaz Azeem P <fayazazeem444555@gmail.com>
2021-02-01 11:07:24 +0530

    Place names capitalized

--- src/components/NearMe/assets/data.js
+++ src/components/NearMe/assets/data.js
@@ -2,15 +2,15 @@ const data = {
     info: [
         { icon: "info", place: "Munnar", minute: "10", km: "5" },
         { icon: "info", place: "Devikulam", minute: "100", km: "50" },
-        { icon: "info", place: "kolukkumalai", minute: "30", km: "12" },
-        { icon: "info", place: "kallar", minute: "50", km: "54" },
+        { icon: "info", place: "Kolukkumalai", minute: "30", km: "12" },
+        { icon: "info", place: "Kallar", minute: "50", km: "54" },
         { icon: "info", place: "Eravikulam", minute: "40", km: "45" },
     ],
     dustbin: [
         { icon: "trash", place: "Munnar", minute: "10", km: "5" },
         { icon: "trash", place: "Devikulam", minute: "100", km: "50" },
-        { icon: "trash", place: "kolukkumalai", minute: "30", km: "12" },
-        { icon: "trash", place: "kallar", minute: "50", km: "54" },
+        { icon: "trash", place: "Kolukkumalai", minute: "30", km: "12" },
+        { icon: "trash", place: "Kallar", minute: "50", km: "54" },
         { icon: "trash", place: "Eravikulam", minute: "40", km: "45" },
     ],
     rest: [
@@ -23,14 +23,14 @@ const data = {
     food: [
         { icon: "food", place: "Munnar", minute: "10", km: "5" },
         { icon: "food", place: "Devikulam", minute: "100", km: "50" },
-        { icon: "food", place: "kolukkumalai", minute: "30", km: "12" },
-        { icon: "food", place: "kallar", minute: "50", km: "54" },
+        { icon: "food", place: "Kolukkumalai", minute: "30", km: "12" },
+        { icon: "food", place: "Kallar", minute: "50", km: "54" },
         { icon: "food", place: "Eravikulam", minute: "40", km: "45" },
     ],
     stay: [
         { icon: "house", place: "Munnar", minute: "10", km: "5" },
         { icon: "house", place: "Devikulam", minute: "100", km: "50" },
-        { icon: "house", place: "kolukkumalai", minute: "30", km: "12" },
+        { icon: "house", place: "Kolukkumalai", minute: "30", km: "12" },
         { icon: "house", place: "Kallar", minute: "50", km: "54" },
         { icon: "house", place: "Eravikulam", minute: "40", km: "45" },
     ],
