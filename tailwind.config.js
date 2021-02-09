a251c1d875f9e9994cb00b8e02c479d2957a81e0
Nikhil Nath <35567322+nikhilnathr@users.noreply.github.com>
2021-02-09 10:52:55 +0530

    Change font to Poppins (from Quicksand) (#86)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         fontFamily: {
-            sans: ["Quicksand"],
+            sans: ["Poppins"],
         },
 
         extend: {
