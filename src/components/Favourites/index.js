073c861dbe308187fe34173b1369a02965c38399
Fayazazeemp <fayazazeem444555@gmail.com>
2021-02-11 21:55:02 +0530

    changes margins

--- src/components/Favourites/index.js
+++ src/components/Favourites/index.js
@@ -6,9 +6,9 @@ export default function Favourite() {
     const [favourite, setFavourite] = useState("Destinations");
     return (
         <div className="bg-gray-50">
-            <p className="text-3xl ml-4 mt-4">Your favourites</p>
+            <p className="text-3xl ml-4 mt-8">Your favourites</p>
             <nav className="px-8 pt-3">
-                <div className="flex justify-center space-x-10 mt-4">
+                <div className="flex justify-center space-x-10 mt-2">
                     <button
                         className="border-b-2 border-transparent text-2xl py-3 focus:outline-none focus:border-black"
                         onClick={() => setFavourite("Destinations")}>
