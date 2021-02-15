3edf6ffc237fd56c55f4da155a2a06be7ea58bbd
Naseem Shah <naseemshahvarikodan@gmail.com>
2021-02-15 22:12:10 +0530

    refactor code for availble days indicator in KSRTC card

--- src/components/Ksrtc/KsrtcCard.js
+++ src/components/Ksrtc/KsrtcCard.js
@@ -2,6 +2,11 @@ import React from "react";
 import clockicon from "./assets/img/alarm-clock.svg";
 
 function KsrtcCard(props) {
+    const days = ["S", "M", "T", "W", "Th", "F", "Sa"];
+    const availableDays =
+        props.availableDays === "allDay"
+            ? days
+            : props.availableDays.split(",");
     return (
         <div className="flex flex-col border-2 border-black w-11/12 bg-white px-7 py-7 p-1 max-w-sm mx-auto m-5 rounded-lg ">
             <div className="mt-0.1">
@@ -24,69 +29,17 @@ function KsrtcCard(props) {
                 </div>
             </div>
             <div className="flex text-center py-2 mt-0.5 space-x-2">
-                <div
-                    className={`flex-1 border-2 border-black ${
-                        props.availableDays === "allDay" ||
-                        props.availableDays.toLowerCase().includes("mon")
-                            ? "bg-black text-white"
-                            : "text-black"
-                    } font-normal text-sm rounded-sm h-6.5 w-6.5`}>
-                    M
-                </div>
-                <div
-                    className={`flex-1 border-2 border-black ${
-                        props.availableDays === "allDay" ||
-                        props.availableDays.toLowerCase().includes("tue")
-                            ? "bg-black text-white"
-                            : "text-black"
-                    } font-normal text-sm rounded-sm h-6.5 w-6.5`}>
-                    T
-                </div>
-                <div
-                    className={`flex-1 border-2 border-black ${
-                        props.availableDays === "allDay" ||
-                        props.availableDays.toLowerCase().includes("wed")
-                            ? "bg-black text-white"
-                            : "text-black"
-                    } font-normal text-sm rounded-sm h-6.5 w-6.5`}>
-                    W
-                </div>
-                <div
-                    className={`flex-1 border-2 border-black ${
-                        props.availableDays === "allDay" ||
-                        props.availableDays.toLowerCase().includes("thu")
-                            ? "bg-black text-white"
-                            : "text-black"
-                    } font-normal text-sm rounded-sm h-6.5 w-6.5`}>
-                    Th
-                </div>
-                <div
-                    className={`flex-1 border-2 border-black ${
-                        props.availableDays === "allDay" ||
-                        props.availableDays.toLowerCase().includes("fri")
-                            ? "bg-black text-white"
-                            : "text-black"
-                    } font-normal text-sm rounded-sm h-6.5 w-6.5`}>
-                    F
-                </div>
-                <div
-                    className={`flex-1 border-2 border-black ${
-                        props.availableDays === "allDay" ||
-                        props.availableDays.toLowerCase().includes("sat")
-                            ? "bg-black text-white"
-                            : "text-black"
-                    } font-normal text-sm rounded-sm h-6.5 w-6.5`}>
-                    Sa
-                </div>
-                <div
-                    className={`flex-1 border-2 border-black ${
-                        props.availableDays === "allDay" ||
-                        props.availableDays.toLowerCase().includes("sun")
-                            ? "bg-black text-white"
-                            : "text-black"
-                    } font-normal text-sm rounded-sm h-6.5 w-6.5`}>
-                    S
-                </div>
+                {days.map((day) => (
+                    <div
+                        key={day}
+                        className={`flex-1 border-2 border-black font-normal text-sm rounded-sm h-6.5 w-6.5 ${
+                            availableDays.includes(day)
+                                ? "bg-black text-white"
+                                : "bg-white text-black"
+                        }`}>
+                        {day}
+                    </div>
+                ))}
             </div>
         </div>
     );
