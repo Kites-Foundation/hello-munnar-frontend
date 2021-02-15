9eb6de99ecb794e67c5a43b92b758e0f8c619d07
Naseem Shah <naseemshahvarikodan@gmail.com>
2021-02-15 22:53:42 +0530

    links ksrtc json with ksrtc page (#113)
    * link ksrtc json with ksrtc page
    
    * refactor code for availble days indicator in KSRTC card
    
    * remove hour 12

--- src/components/Ksrtc/KsrtcCard.js
+++ src/components/Ksrtc/KsrtcCard.js
@@ -2,49 +2,44 @@ import React from "react";
 import clockicon from "./assets/img/alarm-clock.svg";
 
 function KsrtcCard(props) {
+    const days = ["S", "M", "T", "W", "Th", "F", "Sa"];
+    const availableDays =
+        props.availableDays === "allDay"
+            ? days
+            : props.availableDays.split(",");
     return (
-        <div class="flex flex-col border-2 border-black w-11/12 bg-white px-7 py-1 p-1 max-w-sm mx-auto m-5 rounded-lg ">
-            <div class="mt-0.1">
-                <a class="text-xl text-black font-black font-xl" href="/#">
+        <div className="flex flex-col border-2 border-black w-11/12 bg-white px-7 py-7 p-1 max-w-sm mx-auto m-5 rounded-lg ">
+            <div className="mt-0.1">
+                <a className="text-xl text-black font-black font-xl" href="/#">
                     {props.from}-{props.to}
                 </a>
             </div>
-            <div class="flex justify-between items-center mt-1 py-0.5">
-                <div class="flex items-center">
+            <div className="flex justify-between items-center my-3 py-0.5">
+                <div className="flex items-center">
                     <img
                         src={clockicon}
-                        class="w-5 h-5 object-cover rounded-full"
+                        className="w-5 h-5 object-cover rounded-full"
                         alt="clock"></img>
-                    <span class="text-black font-medium text-sm mx-3">
-                        {props.time} {props.amorpm}
+                    <span className="text-black whitespace-nowrap font-medium text-sm mx-3">
+                        {props.time}
                     </span>
-                    <span class="font-medium px-5 text-sm text-black">
+                    <span className="font-medium px-5 text-sm text-black">
                         {props.type}
                     </span>
                 </div>
             </div>
-            <div class="flex text-center py-2 mt-0.5 space-x-2">
-                <div class="flex-1 border-2 border-black bg-black text-white font-normal text-sm text-center rounded-sm h-6.5 w-6.5">
-                    M
-                </div>
-                <div class="flex-1 border-2 border-black bg-black text-white font-normal text-sm rounded-sm h-6.5 w-6.5">
-                    T
-                </div>
-                <div class="flex-1 border-2 border-black bg-black text-white font-normal text-sm rounded-sm h-6.5 w-6.5">
-                    W
-                </div>
-                <div class="flex-1 border-2 border-black bg-black text-white font-normal text-sm rounded-sm h-6.5 w-6.5">
-                    Th
-                </div>
-                <div class="flex-1 border-2 border-black text-black  font-normal text-sm rounded-sm h-6.5 w-6.5">
-                    F
-                </div>
-                <div class="flex-1 border-2 border-black text-black font-normal text-sm rounded-sm h-6.5 w-6.5">
-                    Sa
-                </div>
-                <div class="flex-1 border-2 border-black text-black font-normal text-sm rounded-sm h-6.5 w-6.5">
-                    S
-                </div>
+            <div className="flex text-center py-2 mt-0.5 space-x-2">
+                {days.map((day) => (
+                    <div
+                        key={day}
+                        className={`flex-1 border-2 border-black font-normal text-sm rounded-sm h-6.5 w-6.5 ${
+                            availableDays.includes(day)
+                                ? "bg-black text-white"
+                                : "bg-white text-black"
+                        }`}>
+                        {day}
+                    </div>
+                ))}
             </div>
         </div>
     );
