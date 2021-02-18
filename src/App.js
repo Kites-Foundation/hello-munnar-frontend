d7cb0bfd89834df70fdd67baf80f0d5a5b8fd4d2
gautamkrishnar <r.gautamkrishna@gmail.com>
2021-02-19 03:09:05 +0530

    chore: refactored code, fixed errors

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import ReactGA from "react-ga";
 
 function App() {
     // Initialize GA
-    ReactGA.initialize(Env.GA_ID, { debug: Env.GA_DEBUG });
-    ReactGA.pageview(window.location.pathname + window.location.search);
-    history.listen((location) => {
-        // Intercept route
-        ReactGA.set({ page: location.pathname });
-        ReactGA.pageview(location.pathname + window.location.search);
-    });
+    ReactGA.initialize(process.env.REACT_APP_GA_ID, { debug: false });
     return (
         <div className="App">
             <PublicRouter />
--- src/utils/env.ts
+++
@@ -1,4 +0,0 @@
-export default class Env {
-    static GA_ID = "";
-    static GA_DEBUG = false;
-}
