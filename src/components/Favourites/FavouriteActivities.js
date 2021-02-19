cdd47fde8351cc773017a35fca4adf5758675643
daliyajohn75@gmail.com <daliyajohn75@gmail.com>
2021-02-19 19:32:00 +0530

    feat(theme): update favourite activities layout

--- src/components/Favourites/FavouriteActivities.js
+++ src/components/Favourites/FavouriteActivities.js
@@ -10,36 +10,42 @@ export default function FavouriteActivities() {
         getManyActivity(currentActivityFavourites)
     );
     return (
-        <div className="m-4 ">
-            {activities.map((activity, index) => {
-                return (
-                    <div key={index}>
-                        <FavouriteActivityCard
-                            activityId={activity.activitiesId}
-                            name={activity.activityName || null}
-                            route={
-                                getRoute(activity.activityRouteId)
-                                    .routeColorName || null
-                            }
-                            time={
-                                activity.activityOpenTime ||
-                                activity.activityCloseTime ||
-                                null
-                            }
-                            setActivities={setActivities}
-                            image={activity.bannerImg}
-                        />
-                    </div>
-                );
-            })}
-            {!activities.length && (
-                <>
-                    <h1 className="mt-20 font-medium text-center text-xl ">
-                        Nothing to show here. Please add your favourite
-                        destinations and activities to show up here.
-                    </h1>
-                </>
-            )}
+        <div>
+            <div className="px-8 md:px-10 mt-8 grid grid-cols-1 gap-6 md1:gap-0 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                {activities.map((activity, index) => {
+                    return (
+                        <div
+                            key={index}
+                            className="w-full overflow-hidden bg-white rounded-lg shadow-md cursor-pointer">
+                            <FavouriteActivityCard
+                                activityId={activity.activitiesId}
+                                name={activity.activityName || null}
+                                route={
+                                    getRoute(activity.activityRouteId)
+                                        .routeColorName || null
+                                }
+                                time={
+                                    activity.activityOpenTime ||
+                                    activity.activityCloseTime ||
+                                    null
+                                }
+                                setActivities={setActivities}
+                                image={activity.bannerImg}
+                            />
+                        </div>
+                    );
+                })}
+            </div>
+            <div className="pb-12">
+                {!activities.length && (
+                    <>
+                        <h1 className="mt-20 font-medium text-center text-xl ">
+                            Nothing to show here. Please add your favourite
+                            destinations and activities to show up here.
+                        </h1>
+                    </>
+                )}
+            </div>
         </div>
     );
 }
