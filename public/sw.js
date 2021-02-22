73e5dc578689e845d6d8da4b7526e1669327e836
syam <syam@securityadvisor.io>
2021-02-22 13:46:37 +0530

    update workbox version

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 importScripts(
-    "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js",
+    "https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js",
 );
 
 const HTML_CACHE = "html";
