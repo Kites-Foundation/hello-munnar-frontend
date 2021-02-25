ac9ee86234e0e50d9c979d60baa8c1d19415ca23
Jishnu P Sreekumar <psjishnu13@gmail.com>
2021-02-25 17:15:13 +0530

    added google login

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -7,9 +7,11 @@ import { fireRequest } from "./fireRequest";
 export const login = (body) => {
     return fireRequest("login", [], body);
 };
-*/
-import { fireRequest } from "./fireRequest";
 
-export const googleLogin = () => {
-    return fireRequest("google");
+
+
+export const getCurrentUser = () => {
+    return fireRequest("currentUser");
 };
+
+*/
