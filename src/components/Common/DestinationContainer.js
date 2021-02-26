11f3d9ea594f088ed89f57c07a548f55833c6c64
daliyajohn75@gmail.com <daliyajohn75@gmail.com>
2021-02-26 22:37:34 +0530

    fix(theme): fix carousel space

--- src/components/Common/DestinationContainer.js
+++ src/components/Common/DestinationContainer.js
@@ -4,11 +4,11 @@ import DestinationCard from "./DestinationCard";
 export default function DestinationContainer({ destinations, className = "" }) {
     return (
         <section className={className}>
-            <h2 className="px-8 md:px-10 font-semibold text-black text-xl">
+            <h2 className="mx-8 md:mx-10 font-semibold text-black text-xl">
                 Destinations
             </h2>
 
-            <div className="flex overflow-x-auto py-6 md:py-12 px-8 md:px-10">
+            <div className="flex overflow-x-auto py-6 md:py-12 mx-8 md:mx-10">
                 {destinations.map((destination, id) => (
                     <DestinationCard key={id} data={destination} />
                 ))}
--- src/components/Common/TodoContainer.js
+++ src/components/Common/TodoContainer.js
@@ -6,11 +6,11 @@ export default function TodoContainer({ activities, className = "" }) {
         <section className={className}>
             {activities?.length > 0 && (
                 <>
-                    <h2 className="px-8 md:px-10 font-semibold text-black text-xl">
+                    <h2 className="mx-8 md:mx-10 font-semibold text-black text-xl">
                         Things To Do
                     </h2>
 
-                    <div className="flex overflow-x-auto py-6 md:py-12 px-8 md:px-10">
+                    <div className="flex overflow-x-auto py-6 md:py-12 mx-8 md:mx-10">
                         {activities.map((activity, id) => (
                             <TodoCard key={id} data={activity} />
                         ))}
